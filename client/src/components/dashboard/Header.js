dc42c1654056117d8446f11fb51a456d9b33dd07
Sorya Ek <sorya.ek@spoton.com>
2023-01-14 19:42:32 -0800

    add number next to favorite and watched headers

--- client/src/components/dashboard/Header.js
+++ client/src/components/dashboard/Header.js
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch, faPowerOff } from '@fortawesome/free-solid-svg-icons'
 import { GlobalContext } from "../../context/GlobalState";
+import { padding } from "@mui/system";
 
 export const Header = () => {
   const { watchlist } = useContext(GlobalContext);
   const { watched } = useContext(GlobalContext);
 
   console.log(watchlist);
-
+  
   return (
     <header>
       <div className="container">
@@ -25,28 +26,32 @@ export const Header = () => {
               <Link to="/people">People</Link>
             </li>
           */}
-            <li>
+             <li class="nav-item">
               <Link to="/watchlist">Favorite 
-              {/* {watchlist.length > 0 ? watchlist.length : null} */}
+              {watchlist.length > 0 ? 
+                <span style={{color:'red', paddingLeft:'2px', fontSize:'25px'}}>{watchlist.length}</span>              
+              : null}
               </Link>
             </li>
-            <li>
+            <li class="nav-item">
               <Link to="/watched">Watched 
-              {/* {watched.length > 0 ? watched.length : null} */}
+              {watched.length > 0 ? 
+                <span style={{color:'red', paddingLeft:'2px', fontSize:'25px'}}>{watched.length}</span>              
+              : null}
               </Link>
             </li>
-            <li>
+            <li class="nav-item">
               <Link to="/daily-pick">Daily Pick</Link>
             </li>
-            <li>
+            <li class="nav-item">
               <Link to="/az-list">A-Z List</Link>
             </li>
-            <li>
+            <li class="nav-item">
               <Link to="/SearchAdd" className="search-icon">
                 <FontAwesomeIcon icon={faSearch} />
               </Link>
             </li>
-            <li>
+            <li class="nav-item">
               <Link to="/logout" className="logout-btn">
                 <FontAwesomeIcon icon={faPowerOff} />
               </Link>
