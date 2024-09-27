a9c2e632527d24f7a58a91084583ed5905f65b18
soryaek <sorya.ek@gmail.com>
2024-09-26 21:49:26 -0700

    make video on loop

--- client/src/components/dashboard/Browse.js
+++ client/src/components/dashboard/Browse.js
@@ -9,10 +9,15 @@ const Browse = () => {
         <div>
             <Header />
             <div className="big-trailer-screen">
-                <iframe width="100%" height="450" src="https://www.youtube.com/embed/6ZfuNTqbHE8?si=LYx-IEZLGwrEuB1y&autoplay=1&modestbranding=1&rel=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                    referrerpolicy="strict-origin-when-cross-origin" 
-                    allowfullscreen>
-                </iframe>
+            <iframe width="100%" height="450" 
+                src="https://www.youtube.com/embed/6ZfuNTqbHE8?si=LYx-IEZLGwrEuB1y&autoplay=1&modestbranding=1&rel=0&loop=1&playlist=6ZfuNTqbHE8" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                referrerpolicy="strict-origin-when-cross-origin" 
+                allowfullscreen>
+            </iframe>
+
             </div>
             <div style={{marginTop: '20px'}}>
                 <DisplayMovies type='trending/all/day' />
