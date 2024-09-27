1a630ac17217923644369b2a509a670879616b95
soryaek <sorya.ek@gmail.com>
2024-09-26 21:52:07 -0700

    add header for now playing

--- client/src/components/dashboard/NowPlaying.js
+++ client/src/components/dashboard/NowPlaying.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { DisplayMovies } from './DisplayMovies';
+import { Header } from './Header';
 
 export const NowPlaying = () => {
     return(
         <div>
+          <Header />
           <DisplayMovies type='now_playing' />
         </div> 
     )
